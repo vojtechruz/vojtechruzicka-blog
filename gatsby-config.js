55b65ff9b6463cd52bfa10a363d336644cd51583
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-02-27 13:02:24 +0100

    added feed to match wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,58 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
+      {
+          resolve: `gatsby-plugin-feed`,
+          options: {
+              query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+              feeds: [
+                  {
+                      serialize: ({ query: { site, allMarkdownRemark } }) => {
+                          return allMarkdownRemark.edges.map(edge => {
+                              return Object.assign({}, edge.node.frontmatter, {
+                                  description: edge.node.excerpt,
+                                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                  custom_elements: [{ "content:encoded": edge.node.html }],
+                              });
+                          });
+                      },
+                      query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+                      output: "/feed",
+                  },
+              ],
+          },
+      },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
