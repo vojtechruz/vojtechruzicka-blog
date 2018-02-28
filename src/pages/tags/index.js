e085c04cc2ad2ef9774b4602ba4e58c62bd24b4c
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-02-28 22:01:04 +0100

    Added tags list

---
+++ src/pages/tags/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// Utilities
+import kebabCase from "lodash/kebabcase";
+
+// Components
+import Helmet from "react-helmet";
+import Link from "gatsby-link";
+
+const TagsPage = ({
+                      data: { allMarkdownRemark: { group }, site: { siteMetadata: { title } } },
+                  }) => (
+    <div>
+        <Helmet title={title} />
+        <div>
+            <h1>Tags</h1>
+            <ul>
+                {group.map(tag => (
+                    <li key={tag.fieldValue}>
+                        <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                            {tag.fieldValue} ({tag.totalCount})
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </div>
+);
+
+TagsPage.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            group: PropTypes.arrayOf(
+                PropTypes.shape({
+                    fieldValue: PropTypes.string.isRequired,
+                    totalCount: PropTypes.number.isRequired,
+                }).isRequired
+            ),
+        }),
+        site: PropTypes.shape({
+            siteMetadata: PropTypes.shape({
+                title: PropTypes.string.isRequired,
+            }),
+        }),
+    }),
+};
+
+export default TagsPage;
+
+export const pageQuery = graphql`
+  query TagsQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      limit: 2000
+    ) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
