11fea0231d111362eb036eb9e130d92a658d39ef
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-03-02 17:44:13 +0100

    Added favicon support

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,5 +112,23 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+      {
+          resolve: `gatsby-plugin-favicon`,
+          options: {
+              logo: "./src/favicon.png",
+              injectHTML: true,
+              icons: {
+                  android: true,
+                  appleIcon: true,
+                  appleStartup: true,
+                  coast: false,
+                  favicons: true,
+                  firefox: true,
+                  twitter: false,
+                  yandex: false,
+                  windows: false
+              }
+          }
+      }
   ],
 }
