3b7364707fd7f61f0dd7e22212d5ff34cc6389a1
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-03-02 20:31:42 +0100

    Added bootstrap typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,15 +1,13 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import Bootstrap from 'typography-theme-bootstrap'
 
-Wordpress2016.overrideThemeStyles = () => ({
+Bootstrap.overrideThemeStyles = () => ({
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
   },
 })
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(Bootstrap)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
