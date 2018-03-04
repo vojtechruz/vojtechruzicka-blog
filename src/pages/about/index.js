0e3d30a6d230ec3ded4c866a462abbb521e505fe
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-03-04 12:54:05 +0100

    added icons for about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -21,6 +21,8 @@ const githubIcon = <svg className="about-icon" xmlns="http://www.w3.org/2000/svg
 
 const gPlusIcon = <svg className="about-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M386.061 228.496c1.834 9.692 3.143 19.384 3.143 31.956C389.204 370.205 315.599 448 204.8 448c-106.084 0-192-85.915-192-192s85.916-192 192-192c51.864 0 95.083 18.859 128.611 50.292l-52.126 50.03c-14.145-13.621-39.028-29.599-76.485-29.599-65.484 0-118.92 54.221-118.92 121.277 0 67.056 53.436 121.277 118.92 121.277 75.961 0 104.513-54.745 108.965-82.773H204.8v-66.009h181.261zm185.406 6.437V179.2h-56.001v55.733h-55.733v56.001h55.733v55.733h56.001v-55.733H627.2v-56.001h-55.733z"/></svg>;
 
+const facebookIcon = <svg className="about-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 264 512"><path d="M76.7 512V283H0v-91h76.7v-71.7C76.7 42.4 124.3 0 193.8 0c33.3 0 61.9 2.5 70.2 3.6V85h-48.2c-37.8 0-45.1 18-45.1 44.3V192H256l-11.7 91h-73.6v229"/></svg>;
+
         return (
             <div className="about-page">
                 <Helmet title={siteTitle} />
@@ -43,6 +45,7 @@ const gPlusIcon = <svg className="about-icon" xmlns="http://www.w3.org/2000/svg"
                     <li><a href="https://www.linkedin.com/in/vojtechruzicka">{linkedInIcon}LinkedIn profile</a></li>
                     <li><a href="https://github.com/vojtechruz">{githubIcon}GitHub profile</a></li>
                     <li><a href="http://stackoverflow.com/users/4560142/vojtech-ruzicka">{stackOverflowIcon}Stack Overflow profile</a></li>
+                    <li><a href="https://www.facebook.com/vojtechruzickablog">{facebookIcon}Facebook page</a></li>
                 </ul>
             </div>
         )
