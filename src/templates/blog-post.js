61ce4b1cf549ea1293a2ace6d6a2225e9bef3649
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-03-16 19:27:52 +0100

    Fixed not closed meta description tag

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -62,7 +62,7 @@ class BlogPostTemplate extends React.Component {
       return (
       <div>
           <Helmet title={`${post.frontmatter.title} | ${siteTitle}`}>
-              <meta name="description" content={post.excerpt}>
+              <meta name="description" content={post.excerpt} />
 
               <meta property="og:title" content={post.frontmatter.title} />
               <meta property="og:description" content={post.excerpt} />
