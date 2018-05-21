97eef877a8a5a9bebf2a775bcd3f0381e898f76e
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-05-21 10:21:26 +0200

    Widened the page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,7 +19,7 @@ class Template extends React.Component {
     return (
       <Container
         style={{
-          maxWidth: rhythm(24),
+          maxWidth: rhythm(26),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
