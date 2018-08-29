30c13a2b3ab310682514b4dc59c2c5d229273568
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-08-29 13:30:10 +0200

    docsearch css now does not block rendering

--- src/html.js
+++ src/html.js
@@ -31,10 +31,6 @@ module.exports = class HTML extends React.Component {
           />
           {this.props.headComponents}
           {css}
-            <link
-                rel="stylesheet"
-                href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
-            />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
