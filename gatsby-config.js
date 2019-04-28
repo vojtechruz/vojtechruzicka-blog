ab3556ef24c2b579d6b7b01b93493f98fa40bf67
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2019-04-28 21:51:38 +0200

    Added link icons to post headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
           },
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
+          'gatsby-remark-autolink-headers',
         ],
       },
     },
