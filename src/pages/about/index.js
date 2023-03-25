93b5a8ee5e919e302fee1e9d88c75bb7d6069bfc
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2023-03-25 18:09:11 +0100

    Removed link to google developer profile

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -85,16 +85,6 @@ class AboutPage extends React.Component {
       </svg>
     );
 
-    const googleDevIcon = (
-        <svg
-            className="about-icon"
-            xmlns="https://www.w3.org/2000/svg"
-            viewBox="0 0 370 450"
-        >
-          <path d="M386.061 228.496c1.834 9.692 3.143 19.384 3.143 31.956C389.204 370.205 315.599 448 204.8 448c-106.084 0-192-85.915-192-192s85.916-192 192-192c51.864 0 95.083 18.859 128.611 50.292l-52.126 50.03c-14.145-13.621-39.028-29.599-76.485-29.599-65.484 0-118.92 54.221-118.92 121.277 0 67.056 53.436 121.277 118.92 121.277 75.961 0 104.513-54.745 108.965-82.773H204.8v-66.009h181.261zm185.406" />
-        </svg>
-    );
-
     return (
       <Layout>
         <div className="about-page">
@@ -168,11 +158,6 @@ class AboutPage extends React.Component {
             <li>
               <OutboundLink href="https://www.facebook.com/vojtechruzickablog">
                 {facebookIcon}Facebook page
-              </OutboundLink>
-            </li>
-            <li>
-              <OutboundLink href="https://github.com/vojtechruz">
-                {googleDevIcon}Google Developer Profile
               </OutboundLink>
             </li>
           </ul>
