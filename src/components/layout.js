76f436a29d03d524b22bdd454a02a78a533e0f9e
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2023-09-23 22:26:40 +0200

    Gatsby v2 to v3 upgrade: Removed unused code causing issues

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,11 +10,6 @@ class Template extends React.Component {
   render() {
     const { children } = this.props;
 
-    let rootPath = `/`;
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`;
-    }
-
     return (
       <div className="content-root"
         style={{
