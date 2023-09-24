9f3fc48ab3d936cfb6c02feb9c272a9515a1d804
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2023-09-24 20:18:22 +0200

    Gatsby v2 to v3 upgrade: Images migration

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
                 name: 'posts',
             },
         },
+        "gatsby-plugin-image",
         {
             resolve: `gatsby-transformer-remark`,
             options: {
