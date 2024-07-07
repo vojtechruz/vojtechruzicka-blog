651b0ae5863eb9aac76ba0e73ba2512df4147470
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2024-07-08 00:14:10 +0200

    removed deprecated universal analytics tracking id

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -104,8 +104,7 @@ const config = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "UA-76533683-1", // Google Analytics 3
-          "G-5SJ6ERGH23", // Google Analytics 4
+          "G-5SJ6ERGH23" // Google Analytics 4
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
