f9e829ba38168dbe73063f3eda1aaf095ac430a5
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2025-10-20 00:15:54 +0200

    Eleventy Migration - added config file

---
+++ _eleventy/eleventy.config.mjs
@@ -0,0 +1,5 @@
+export default async function (eleventyConfig) {
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setIncludesDirectory("_includes");// default
+  eleventyConfig.setOutputDirectory("_site");// default
+}
