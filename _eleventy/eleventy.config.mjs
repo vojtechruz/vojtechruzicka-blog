e082100d643f9493677b95280869ec6c8dd3c514
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2025-10-20 23:28:06 +0200

    Eleventy Migration - pagination

--- _eleventy/eleventy.config.mjs
+++ _eleventy/eleventy.config.mjs
@@ -1,7 +1,13 @@
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
-      "./src/static/": "/" // Copy static files to the output directory
-    })
+    "./src/static/": "/" // Copy static files to the output directory
+  });
+
+  eleventyConfig.addCollection("posts", (api) =>
+    api.getFilteredByGlob("src/posts/**/*.md")
+      .filter(it => !it.data.isDraft)
+      .sort((a, b) => b.date - a.date)
+  );
 
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
