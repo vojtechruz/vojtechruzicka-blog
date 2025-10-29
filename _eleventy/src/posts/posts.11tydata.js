7a786b4cdf62815c453d15fddaf62a73a5c5229f
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2025-10-29 23:48:47 +0100

    Eleventy Migration - Proper image support on main page

--- _eleventy/src/posts/posts.11tydata.js
+++ _eleventy/src/posts/posts.11tydata.js
@@ -1,7 +1,11 @@
+import path from "path";
+
 export default {
   layout: "layouts/post.njk",
   eleventyComputed: {
     // If Markdown still uses `path` (from Gatsby), treat it as Eleventy's permalink
     permalink: data => data.path || data.permalink,
+    featuredImage: data => data.featuredImage || "featured.jpg",
+    postDir: (data) => path.dirname(data.page.inputPath),
   }
 };
