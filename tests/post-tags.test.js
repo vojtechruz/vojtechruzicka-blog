1eed6de71fd49ac47c5344bd78d52ce20b7ac30c
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2026-04-06 23:26:29 +0200

    Eleventy Migration - Fixed mismatch between tag names on linux and windows

---
+++ tests/post-tags.test.js
@@ -0,0 +1,36 @@
+import { describe, it, expect } from 'vitest';
+import { loadPage, getAllPosts } from './helpers.js';
+import { existsSync } from 'fs';
+
+describe('Post tag links', () => {
+  const posts = getAllPosts();
+
+  it('tag links use lowercase slugified hrefs with trailing slash', () => {
+    for (const { frontmatter } of posts) {
+      if (!frontmatter.path || !frontmatter.tags) continue;
+
+      const $ = loadPage(frontmatter.path);
+      const tagLinks = $('ul.post-tags a.tag-name');
+
+      tagLinks.each((_, el) => {
+        const href = $(el).attr('href');
+        expect(href).toMatch(/^\/tags\/[a-z0-9-]+\/$/);
+      });
+    }
+  });
+
+  it('tag links point to existing tag pages', () => {
+    for (const { frontmatter } of posts) {
+      if (!frontmatter.path || !frontmatter.tags) continue;
+
+      const $ = loadPage(frontmatter.path);
+      const tagLinks = $('ul.post-tags a.tag-name');
+
+      tagLinks.each((_, el) => {
+        const href = $(el).attr('href');
+        const tagPagePath = `_site${href}index.html`;
+        expect(existsSync(tagPagePath), `Tag page missing: ${tagPagePath}`).toBe(true);
+      });
+    }
+  });
+});
